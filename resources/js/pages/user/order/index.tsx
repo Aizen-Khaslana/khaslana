3bc918256cddf72fb198ed80fb531de108a642da
Fajri Bagaskara Budi <fajribagaskara69@gmail.com>
2026-06-02 21:34:54 +0700

    feat: added order page

--- resources/js/pages/user/order/index.tsx
+++ resources/js/pages/user/order/index.tsx
@@ -13,7 +13,7 @@ export default function Order() {
 
     return (
         <UnusedNavLayout backHref={backUrl}>
-            <Head title='Komunitas'>
+            <Head title='Order Produk'>
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
                     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
