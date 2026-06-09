82a774cf2e8fa16497e5611bf5a0b136b649cd74
rafidhp <rafiislamipasha27@gmail.com>
2026-06-10 00:30:53 +0700

    fix: dashboard data

--- app/Http/Controllers/UmkmController.php
+++ app/Http/Controllers/UmkmController.php
@@ -94,7 +94,7 @@ class UmkmController extends Controller
     {
         $userLat = $request->lat;
         $userLng = $request->lng;
-        $radius = 200;
+        $radius = 3;
         $activeMerchants = Umkm::where('type', 'KELILING')
             ->whereHas('umkmLocations', function ($q) {
                 $q->where('is_active', true);
